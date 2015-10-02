8055cf1b598cfee87425931206731b6d37c29dfa
alyyeo <alyssalyyeo@gmail.com>
2015-10-02 15:50:03 +0000

    checkpoint 42 posts and users completed

--- lib/random_data.rb
+++ lib/random_data.rb
@@ -21,4 +21,14 @@ module RandomData
         end
         sentences.join(".")
     end
+    
+    def random_name
+        first_name = random_word.capitalize
+        last_name = random_word.capitalize
+        "#{first_name} #{last_name}"
+    end
+    
+    def random_email
+        "#{random_word}@#{random_word}.#{random_word}"
+    end
 end
