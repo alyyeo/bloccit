34a42994bef3e070f9cfb5a7889b2f6e05667de7
alyyeo <alyssalyyeo@gmail.com>
2015-10-12 20:49:50 +0000

    assignment 49 completed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+    def any_posts?
+        @user.posts.any?
+    end
+    
+    def any_comments?
+        @user.comments.any?
+    end
+    
+    def any_favorites?
+        @user.favorites.any?
+    end
 end
--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Favorite, type: :model do
     let(:topic) { create(:topic) }
     let(:user) { create(:user) }
     let(:post) { create(:post) }
-    let(:favorite) { Favorite.create!(post: post, user: user) }
+    let(:favorite) { create(:favorite) }
     
     it { should belong_to(:post) }
     it { should belong_to(:user) }
