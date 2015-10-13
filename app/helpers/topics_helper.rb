aa266f75a77a5506e59aaafb33918aec92db2cca
alyyeo <alyssalyyeo@gmail.com>
2015-10-13 03:55:35 +0000

    assignment 50 completed

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -2,4 +2,12 @@ module TopicsHelper
     def user_is_authorized_for_topics?
         current_user && current_user.admin?
     end
+    
+    def topic_type_button
+        if @topic.public
+            link_to "Public", topics_path(:scope => "publicly_viewable"), class: 'btn-xs btn-success'
+        else
+            link_to "Private", topics_path(:scope => "privately_viewable"), class: 'btn-xs btn-danger'
+        end
+    end
 end
